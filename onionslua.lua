c2e09d3dbb6c5e1e2fb2dd2420f75cc0cd5f63b3
onion <62121953+oniongithub@users.noreply.github.com>
2021-12-12 03:26:22 -0500

    Update onionslua.lua

--- onionslua.lua
+++ onionslua.lua
@@ -1,5 +1,5 @@
 local ffi, vector, http, images = require("ffi"), require("vector"), require("gamesense/http"), require("gamesense/images")
-local init, localPlayer, mousePos, dpi, version = true, entity.get_local_player(), nil, nil, "4q02EIO1cg7qRcAK"
+local init, localPlayer, mousePos, dpi, version = true, entity.get_local_player(), nil, nil, "8IMKSx77kuZGhuBt"
 local menuR, menuG, menuB, menuA = ui.get(ui.reference("Misc", "Settings", "Menu color"))
 local screenSize, menuPos, menuSize = vector(client.screen_size()), vector(ui.menu_position()), vector(ui.menu_size())
 
