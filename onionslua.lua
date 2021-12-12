96f311db802f83ccdbcec98e49cdb3827a0d088c
onion <62121953+oniongithub@users.noreply.github.com>
2021-12-12 03:25:54 -0500

    Update onionslua.lua

--- onionslua.lua
+++ onionslua.lua
@@ -1,5 +1,5 @@
 local ffi, vector, http, images = require("ffi"), require("vector"), require("gamesense/http"), require("gamesense/images")
-local init, localPlayer, mousePos, dpi, version = true, entity.get_local_player(), nil, nil, "ov31MLwdqcIu9qIy"
+local init, localPlayer, mousePos, dpi, version = true, entity.get_local_player(), nil, nil, "4q02EIO1cg7qRcAK"
 local menuR, menuG, menuB, menuA = ui.get(ui.reference("Misc", "Settings", "Menu color"))
 local screenSize, menuPos, menuSize = vector(client.screen_size()), vector(ui.menu_position()), vector(ui.menu_size())
 
@@ -1952,9 +1952,11 @@ client.set_event_callback("player_death", function(e)
 
     playerKilledEvent(e)
 
+    --[[
     if (ent == localPlayer) then
         onionQuickPeekEvent(e)
     end
+    --]]
 end)
 
 client.set_event_callback("round_prestart", function(e)
